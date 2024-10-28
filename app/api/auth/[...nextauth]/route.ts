35a1508ece6646bce117a9f0068524e91067a91a
MaheswarReddy <palugullavenkatamaheswarreddy@gmail.com>
2024-10-29 01:10:52 +0530

    Added github authentication

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+// The correct export method for Next.js 13 App Router
+export { handler as GET, handler as POST };
--- app/signin/layout.tsx
+++ app/signin/layout.tsx
@@ -13,7 +13,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-       <div>hettt</div>
         {children}
       </body>
     </html>
